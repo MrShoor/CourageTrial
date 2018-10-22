3549fe6ce365507fdf7072d438af011bfea47496
mrshoor <mrshoor@yandex.ru>
2018-10-22 00:26:15 -0700

    roomeditor project added

---
+++ untroomeditormain.pas
@@ -0,0 +1,29 @@
+unit untRoomEditorMain;
+
+{$IfDef FPC}
+  {$mode objfpc}{$H+}
+  {$ModeSwitch advancedrecords}
+{$EndIf}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+type
+  TfmrMain = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  fmrMain: TfmrMain;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
